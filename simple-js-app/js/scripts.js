ceb80d6f2fbbcff765d99187398fecab89d2e14b
rubenfinol <114367069+rubenfinol@users.noreply.github.com>
2023-04-05 19:27:56 -0400

    Add files via upload

---
+++ simple-js-app/js/scripts.js
@@ -0,0 +1,12 @@
+let myName = 'Bob';
+document.write(myName);
+myName = 'Jhon doe';
+document.write(myName);
+
+let simpleAdittion = 2 + 2;
+document.write(simpleAdittion);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
